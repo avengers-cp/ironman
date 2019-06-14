acf757ae90696c3ddd101fd59b410f6588c23597
Miten Chauhan <miten.chauhan@safer.me>
2019-06-14 18:42:53 +1200

    Convert to reactive form

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  public loginForm: FormGroup;
+
+  constructor(private formbuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.initForm();
+  }
+
+  public onSubmit() {
+    console.log('--- submit clicked');
+  }
+
+  private initForm() {
+    this.loginForm = this.formbuilder.group({
+      email: [
+        '',
+        Validators.compose([Validators.email, Validators.required])
+      ],
+      password: [
+        '',
+        Validators.compose([Validators.minLength(6), Validators.required])
+      ]
+    });
   }
 
 }
