ef945f5ca1f58b3b772cce3901deac3f92acf276
Miten Chauhan <miten.chauhan@safer.me>
2019-06-14 22:40:53 +1200

    Expand toast service to show error toasts.

---
+++ src/app/interfaces/toast-config.ts
@@ -0,0 +1,23 @@
+// Extract from https://ionicframework.com/docs/api/toast to help make strongly typed.
+
+import { ToastButton } from '@ionic/core';
+
+type DeviceMode = 'ios' | 'md';
+type ToastPosition = 'bottom' | 'middle' | 'top';
+
+export interface ToastConfig {
+  animated?: boolean;
+  buttons?: (string | ToastButton)[];
+  closeButtonText?: string;
+  color?: string;
+  cssClass?: string | string[];
+  duration?: number;
+  header?: string;
+  keyboardClose?: boolean;
+  message?: string;
+  mode?: DeviceMode;
+  position?: ToastPosition;
+  showCloseButton?: boolean;
+  translucent?: boolean;
+  [key: string]: any;
+}
--- src/app/services/toast.service.ts
+++ src/app/services/toast.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ToastController } from '@ionic/angular';
+import { ToastConfig } from '../interfaces/toast-config';
 
 @Injectable({
   providedIn: 'root'
@@ -9,16 +10,41 @@ export class ToastService {
   constructor(private toastCtrl: ToastController) { }
 
   /**
+   * Show an error toast for a set amount of time.
+   * @param message - The error message to be displayed.
+   * @param duration - The duration to display the toast for. Default is 2000 milliseconds.
+   */
+  public showErrorToast(message: string, duration: number = 2000): Promise<void> {
+    const toastConfig: ToastConfig = {
+      message,
+      duration,
+      keyboardClose: true,
+      color: 'danger'
+    };
+    return this.createToast(toastConfig);
+  }
+
+  /**
    * Show a basic info toast for a set amount of time.
    * @param message - The message to be displayed on the toast.
    * @param duration - The duration to display the toast for. Default is 2000 milliseconds.
    */
-  public async showInfoToast(message: string, duration: number = 2000): Promise<void> {
-    const toast = await this.toastCtrl.create({
+  public showInfoToast(message: string, duration: number = 2000): Promise<void> {
+    const toastConfig: ToastConfig = {
       message,
       duration,
       keyboardClose: true
-    });
+    };
+    return this.createToast(toastConfig);
+  }
+
+  /**
+   * Create a toast.
+   * @param toastConfig - Settings to configure the toast.
+   * @returns - Resolves when the toast is created.
+   */
+  private async createToast(toastConfig: ToastConfig): Promise<void> {
+    const toast = await this.toastCtrl.create(toastConfig);
     await toast.present();
   }
 }
