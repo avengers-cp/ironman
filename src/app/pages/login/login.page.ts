7e44bdf247df9d103c29c53469b19e00f13c38be
Miten Chauhan <miten.chauhan@safer.me>
2019-06-14 22:52:03 +1200

    Show error toasts

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,5 +1,9 @@
+import { AuthService } from './../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ToastService } from 'src/app/services/toast.service';
+import { LoadingController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,16 +14,39 @@ export class LoginPage implements OnInit {
 
   public loginForm: FormGroup;
 
-  constructor(private formbuilder: FormBuilder) { }
+  constructor(
+    private authService: AuthService,
+    private formbuilder: FormBuilder,
+    private loadingCtrl: LoadingController,
+    private router: Router,
+    private toastService: ToastService
+  ) { }
 
   ngOnInit() {
     this.initForm();
   }
 
-  public onSubmit() {
-    console.log('--- submit clicked');
+  public async onSubmit(): Promise<void> {
+    if (this.loginForm.valid) {
+      const loading = await this.loadingCtrl.create();
+      loading.present();
+      const { email, password } = this.loginForm.value;
+      try {
+        await this.authService.login(email, password);
+        this.router.navigateByUrl('/tabs/tab1');
+      } catch (error) {
+        this.toastService.showErrorToast('Invalid email or password. Please try again');
+      } finally {
+        loading.dismiss();
+      }
+    } else {
+      this.toastService.showInfoToast('Please ensure all fields have been filled out correctly');
+    }
   }
 
+  /**
+   * Initialise the login form.
+   */
   private initForm() {
     this.loginForm = this.formbuilder.group({
       email: [
@@ -28,7 +55,7 @@ export class LoginPage implements OnInit {
       ],
       password: [
         '',
-        Validators.compose([Validators.minLength(6), Validators.required])
+        Validators.required
       ]
     });
   }
