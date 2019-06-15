4f5b82096bb1cdcca64c46770bc5a70a50131cea
Miten Chauhan <miten.chauhan@safer.me>
2019-06-15 16:46:04 +1200

    Create a firebase user

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -9,6 +9,33 @@ export class AuthService {
   constructor(private afAuth: AngularFireAuth) { }
 
   /**
+   * Create a user account.
+   * @param birthdate - Users date of birth.
+   * @param email - Users email.
+   * @param firstname - Users first name.
+   * @param lastname - Users last name.
+   * @param password - Users password.
+   * @returns - Resolves when the user account has been created.
+   */
+  public createUser(
+    birthdate: Date,
+    email: string,
+    firstname: string,
+    lastname: string,
+    password: string
+  ): Promise<void> {
+    return new Promise<void>(async (resolve, reject) => {
+      try {
+        const userCredential = await this.afAuth.auth.createUserWithEmailAndPassword(email, password);
+        // TODO: call user service method to create userprofile.
+        resolve();
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
+
+  /**
    * Login to the app with an email and password.
    * @param email - User's email.
    * @param password - User's password.
