6562b96fd2d1533f727560ebdbe559066b79ac12
Miten Chauhan <miten.chauhan@safer.me>
2019-06-18 21:49:22 +1200

    Show create exercise button if you are an admin

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,11 @@
+import { User } from './../interfaces/user';
+import { UserService } from './../services/user.service';
 import { AuthService } from './../services/auth.service';
 import { Component } from '@angular/core';
 import { NavController, LoadingController } from '@ionic/angular';
+
+import * as _ from 'lodash';
+import { Role } from '../interfaces/role.enum';
 
 @Component({
   selector: 'app-tab1',
@@ -12,8 +17,17 @@ export class Tab1Page {
   constructor(
     private authService: AuthService,
     private loadingCtrl: LoadingController,
-    private navCtrl: NavController
+    private navCtrl: NavController,
+    private userService: UserService
   ) { }
+
+  public get canCreateExercise(): boolean {
+    return _.get(this.user, 'role') === Role.MASTER_ADMIN;
+  }
+
+  public get user(): User {
+    return this.userService.user;
+  }
 
   public async logout(): Promise<void> {
     const loading = await this.loadingCtrl.create();
@@ -22,5 +36,4 @@ export class Tab1Page {
     this.navCtrl.navigateRoot('/login');
     loading.dismiss();
   }
-
 }
