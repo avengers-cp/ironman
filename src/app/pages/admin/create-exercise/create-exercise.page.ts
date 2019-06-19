157257b9544d8402e84fff4ff21b0118f81d8fae
Miten Chauhan <miten.chauhan@safer.me>
2019-06-20 11:48:54 +1200

    Initialise all our values

--- src/app/pages/admin/create-exercise/create-exercise.page.ts
+++ src/app/pages/admin/create-exercise/create-exercise.page.ts
@@ -1,5 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { EquipmentType } from './../../../models/equipment-type.enum';
+import {
+  AbMuscle,
+  ArmMuscle,
+  BackMuscle,
+  ChestMuscle,
+  LegMuscle,
+  MainMuscleGroup,
+  ShoulderMuscle
+} from './../../../models/muscle';
+
+import * as _ from 'lodash';
+
 @Component({
   selector: 'app-create-exercise',
   templateUrl: './create-exercise.page.html',
@@ -7,9 +20,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateExercisePage implements OnInit {
 
-  constructor() { }
+  public mainMuscleGroups: string[];
+
+  public abMuscles: string[];
+  public armMuscles: string[];
+  public backMuscles: string[];
+  public chestMuscles: string[];
+  public legMuscles: string[];
+  public shoulderMuscles: string[];
+
+  public equipmentTypes: string[];
+
+  constructor() {
+    window['ce'] = this;
+  }
 
   ngOnInit() {
+    this.mainMuscleGroups = _.values(MainMuscleGroup);
+
+    this.abMuscles = _.values(AbMuscle);
+    this.armMuscles = _.values(ArmMuscle);
+    this.backMuscles = _.values(BackMuscle);
+    this.chestMuscles = _.values(ChestMuscle);
+    this.legMuscles = _.values(LegMuscle);
+    this.shoulderMuscles = _.values(ShoulderMuscle);
+
+    this.equipmentTypes = _.values(EquipmentType);
   }
 
 }
