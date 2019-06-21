7cc303a193675008594926e2c7834414e956221d
Miten Chauhan <miten.chauhan@safer.me>
2019-06-21 20:35:00 +1200

    Add more functionality to the exercises service

--- src/app/services/exercise.service.ts
+++ src/app/services/exercise.service.ts
@@ -1,4 +1,5 @@
-import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 import { Injectable } from '@angular/core';
 import { Exercise } from '../models/exercise';
 import { Muscle } from '../models/muscle';
@@ -9,7 +10,13 @@ import { EquipmentType } from '../models/equipment-type.enum';
 })
 export class ExerciseService {
 
-  constructor(private firestore: AngularFirestore) { }
+  public exercises$: Observable<Exercise[]>;
+  private exercisesCollection: AngularFirestoreCollection<Exercise>;
+
+  constructor(private firestore: AngularFirestore) {
+    this.exercisesCollection = this.firestore.collection<Exercise>('exercises');
+    this.exercises$ = this.exercisesCollection.valueChanges();
+  }
 
   /**
    * Create an exercise and add it to the list of exercises in the database.
@@ -26,6 +33,24 @@ export class ExerciseService {
       name,
       targetedMuscles
     };
-    return this.firestore.doc<Exercise>(`exercises/${id}`).set(exercise);
+    return this.exercisesCollection.doc<Exercise>(id).set(exercise);
+  }
+
+  /**
+   * Delete an exercise from the database.
+   * @param exerciseId - The ID of the exercise to delete.
+   * @returns - Resolves a promise when the exercise has been deleted.
+   */
+  public deleteExercise(exerciseId: string): Promise<void> {
+    return this.exercisesCollection.doc<Exercise>(exerciseId).delete();
+  }
+
+  /**
+   * Update the exercise object in the database.
+   * @param exercise - The exercise object that has been updated.
+   * @returns - Resolves a promise when the exercise has been updated.
+   */
+  public updateExercise(exercise: Exercise): Promise<void> {
+    return this.exercisesCollection.doc<Exercise>(exercise.id).set(exercise, {merge: true});
   }
 }
