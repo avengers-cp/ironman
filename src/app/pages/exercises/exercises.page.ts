42b72df572c5c0d9a3b0a32c4b65b8050cf5e190
Miten Chauhan <miten.chauhan@safer.me>
2019-06-22 16:30:25 +1200

    style

--- src/app/pages/exercises/exercises.page.ts
+++ src/app/pages/exercises/exercises.page.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
 import { Exercise } from 'src/app/models/exercise';
 import { ExerciseService } from 'src/app/services/exercise.service';
 
@@ -8,21 +8,26 @@ import { ExerciseService } from 'src/app/services/exercise.service';
   templateUrl: './exercises.page.html',
   styleUrls: ['./exercises.page.scss'],
 })
-export class ExercisesPage implements OnInit {
+export class ExercisesPage implements OnDestroy, OnInit {
 
   public exercises: Exercise[];
 
-  constructor(private exerciseService: ExerciseService) {
-    this.exercises$.subscribe((exercises: Exercise[]) => {
-      this.exercises = exercises;
-    });
-  }
+  private exercisesSubscription: Subscription;
+
+  constructor(private exerciseService: ExerciseService) { }
 
   public get exercises$(): Observable<Exercise[]> {
     return this.exerciseService.exercises$;
   }
 
+  ngOnDestroy() {
+    this.exercisesSubscription.unsubscribe();
+  }
+
   ngOnInit() {
+    this.exercisesSubscription = this.exercises$.subscribe((exercises: Exercise[]) => {
+      this.exercises = exercises;
+    });
   }
 
 }
